e9b8eda326b4ee3d1e42c8785dc79742455a052f
maynkj <mayn.kjaer@gmail.com>
2014-05-16 17:01:25 -0300

    Add validation to signup.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'companies/new'
-  get 'companies/:id', to: 'companies#show', as: "company"
-  post 'companies', to: 'companies#create', as: 'companies'
+  resources :companies, except: [:index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
