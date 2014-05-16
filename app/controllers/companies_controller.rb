c028ba2775f6900ac35ab2b632b6dedb644268ac
maynkj <mayn.kjaer@gmail.com>
2014-05-16 16:00:26 -0300

    Add signup for companies.

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,23 @@
+class CompaniesController < ApplicationController
+  def new
+  end
+
+  def create
+    @company = Company.new(company_params)
+
+    if @company.save
+      redirect_to @company
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @company = Company.find(params[:id])
+  end
+
+  private
+    def company_params
+      params.require(:company).permit(:name, :email, :url, :password, :password_confirmation)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'companies/new'
+  get 'companies/:id', to: 'companies#show', as: "company"
+  post 'companies', to: 'companies#create', as: 'companies'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
