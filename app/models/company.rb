9581b2102b30dd917579dd9e321375b76c40c4ca
maynkj <mayn.kjaer@gmail.com>
2014-05-20 12:40:06 -0300

    Add Post model.

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,5 @@
 class Company < ActiveRecord::Base
+  has_many :posts
   has_secure_password
 
   validates :name, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140516163056) do
+ActiveRecord::Schema.define(version: 20140520153539) do
 
   create_table "companies", force: true do |t|
     t.string   "name",            null: false
@@ -21,5 +21,15 @@ ActiveRecord::Schema.define(version: 20140516163056) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.integer  "company_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["company_id"], name: "index_posts_on_company_id"
 
 end
